8457cd0667b1a5b62f0ee63bd5efb1fd69e1c894
Manish Verma <manishvrm8877@gmail.com>
2025-12-05 19:54:48 +0530

    Some Changes in heading

--- src/Splash.java
+++ src/Splash.java
@@ -10,8 +10,6 @@ public class Splash extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(null);
 
-
-
         heading = new JLabel("INVENTORY MANAGEMENT SYSTEM", SwingConstants.CENTER);
         heading.setBounds(0, 0, 1200, 60);
         heading.setOpaque(true);
